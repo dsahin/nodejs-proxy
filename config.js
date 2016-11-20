7a0ce77131e19d1379b746bec64f899a55e7ccd6
dsahin <dsahin@gmail.com>
2016-11-21 00:19:10 +0300

    2nd

--- config.js
+++ config.js
@@ -13,25 +13,25 @@ var config = {
   allow_ip_list: './config/allow_ip_list',
   black_list:    './config/black_list',
   host_filters:   './config/hostfilters.js',
-  listen:[{ip:'0.0.0.0', port:80},//all ipv4 interfaces
-          {ip:'::', port:80}],//all ipv6 interfaces
-  listen_ssl:[{
-              ip:'0.0.0.0',//all *secure* ipv4 interfaces
-              port:443,
-              key:fs.readFileSync('/path/to/ssl.key'),
-              cert:fs.readFileSync('/path/to/ssl.crt'),
-              ca:[fs.readFileSync('/path/to/ca.pem'), 
-                  fs.readFileSync('/path/to/sub-ca.pem')]
-            },{ 
-              ip:'::',//all *secure* ipv6 interfaces
-              port:443,
-              key:fs.readFileSync('/path/to/ssl.key'),
-              cert:fs.readFileSync('/path/to/ssl.crt'),
-              ca:[fs.readFileSync('/path/to/ca.pem'), 
-                  fs.readFileSync('/path/to/sub-ca.pem')]
+  listen:[{ip:'0.0.0.0', port:8080},//all ipv4 interfaces
+          {ip:'::', port:8080}],//all ipv6 interfaces
+  // listen_ssl:[{
+  //             ip:'0.0.0.0',//all *secure* ipv4 interfaces
+  //             port:443,
+  //             key:fs.readFileSync('/path/to/ssl.key'),
+  //             cert:fs.readFileSync('/path/to/ssl.crt'),
+  //             ca:[fs.readFileSync('/path/to/ca.pem'), 
+  //                 fs.readFileSync('/path/to/sub-ca.pem')]
+  //           },{ 
+  //             ip:'::',//all *secure* ipv6 interfaces
+  //             port:443,
+  //             key:fs.readFileSync('/path/to/ssl.key'),
+  //             cert:fs.readFileSync('/path/to/ssl.crt'),
+  //             ca:[fs.readFileSync('/path/to/ca.pem'), 
+  //                 fs.readFileSync('/path/to/sub-ca.pem')]
 
-            }   
-           ]
+  //           }   
+  //          ]
 };
 
 exports.config = config;
